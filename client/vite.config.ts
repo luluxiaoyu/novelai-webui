22a7f4f196e0d0a946215309b557c91bd0a369c6
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-08-24 21:27:56 +0800

    perf: 优化env导入

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: path.resolve(import.meta.dirname, '..'),
   plugins: [vue()],
   resolve: {
     alias: {
